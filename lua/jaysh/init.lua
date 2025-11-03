b1a6acd30d57b80bc4bb8d26202c03f59fb6bce8
jayshozie <143549527+jayshozie@users.noreply.github.com>
2025-11-03 10:50:02 +0300

    vim.highlight deprecated, switched to vim.hl in lua/jaysh/init.lua

--- lua/jaysh/init.lua
+++ lua/jaysh/init.lua
@@ -79,7 +79,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
     group = vim.api.nvim_create_augroup('kicstart-hightlight-yank', { clear = true }),
     callback  = function()
-        vim.highlight.on_yank()
+        vim.hl.on_yank()
     end,
 })
 
