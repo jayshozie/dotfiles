af4c89488963a0f66b7567baa8722599702200ea
Emir Baha Yıldırım <jayshozie@gmail.com>
2026-05-09 16:34:04 +0300

    wip: automated dev-commit

--- modules/scripts/payload/waybar-net.sh
+++ modules/scripts/payload/waybar-net.sh
@@ -20,15 +20,16 @@
 # {if wifi connected: 󰖩 wifi_strength} {if ethernet connected: 󰈀 <something>}
 
 # interface names for when thunderbolt ethernet connection is available
-tb_back="enp128s20f0u1"
-tb_front="enp128s20f0u3"
-rj45_back="enp129s0"
-css_class="net"
+tb_back='enp128s20f0u1'
+tb_front='enp128s20f0u3'
+rj45_back='enp129s0'
+css_class='net'
+secret='FiberHGW_TP8998'
 
 while true; do
-    wifi_text=""
-    ethernet_text=""
-    tooltip=""
+    wifi_text=''
+    ethernet_text=''
+    tooltip=''
 
     ###########
     #  Wi-fi  #
@@ -40,39 +41,38 @@ while true; do
     if [[ -n "$nmcli_output" ]]; then
         # Format from nmcli -t is: yes:SSID:▂▄▆_
         wifi_ssid=$(echo "$nmcli_output" | cut -d':' -f2)
-        if [[ "$wifi_ssid" == 'FiberHGW_TP8998' ]]; then
-            wifi_ssid=''
-        fi
         wifi_signal=$(echo "$nmcli_output" | cut -d':' -f3)
 
         if [[ "$wifi_signal" -gt 80 ]]; then
-            wifi_bars="▂▄▆█"
+            wifi_bars='▂▄▆█'
         elif [[ "$wifi_signal" -gt 60 ]]; then
-            wifi_bars="▂▄▆_"
+            wifi_bars='▂▄▆_'
         elif [[ "$wifi_signal" -gt 40 ]]; then
-            wifi_bars="▂▄__"
+            wifi_bars='▂▄__'
         elif [[ "$wifi_signal" -gt 20 ]]; then
-            wifi_bars="▂___"
+            wifi_bars='▂___'
         else
-            wifi_bars="____"
+            wifi_bars='____'
         fi
 
         # DEBUG:
         # echo $nmcli_output > ${HOME}/waybar-net-debug.log
 
-        if [[ "$wifi_ssid" == '' ]]; then
+        if [[ $wifi_ssid == "$secret" ]]; then
             wifi_text="󰖩  $wifi_bars"
+        elif [[ $wifi_ssid == '' ]]; then
+            wifi_text=''
         else
             wifi_text="󰖩  $wifi_ssid  $wifi_bars"
         fi
-        if [[ "$wifi_ssid" == '' ]]; then
+        if [[ $wifi_ssid == '' ]]; then
             tooltip=''
         else
             tooltip="Wi-Fi: $wifi_ssid"
         fi
     else
-        wifi_text=""
-        tooltip=""
+        wifi_text=''
+        tooltip=''
     fi
 
     ##############
@@ -80,7 +80,7 @@ while true; do
     ##############
     for iface in $(ls /sys/class/net); do
         # --- Filters ---
-        [[ "$iface" == "lo" ]] && continue
+        [[ $iface == "lo" ]] && continue
         [[ -d "/sys/class/net/${iface}/wireless" ]] && continue
         [[ ! -L "/sys/class/net/${iface}/device" ]] && continue
 
@@ -91,27 +91,32 @@ while true; do
         # --- Mapping ---
         case "$iface" in
             "$tb_back")
-                ethernet_text="  Rear"
+                ethernet_text='  Rear'
                 tooltip="${tooltip} | Ethernet: Thunderbolt Rear"
                 ;;
             "$tb_front")
-                ethernet_text="  Front"
+                ethernet_text='  Front'
                 tooltip="${tooltip} | Ethernet: Thunderbolt Front"
                 ;;
             "$rj45_back")
-                ethernet_text="󰈀  RJ45"
+                ethernet_text='󰈀  RJ45'
                 tooltip="${tooltip} | Ethernet: RJ45 Back"
                 ;;
             *)
-                ethernet_text="󰈀  Ext"
+                ethernet_text='󰈀  Ext'
                 tooltip="${tooltip} | Ethernet: External ($iface)"
                 ;;
         esac
-        ethernet_text=" ${ethernet_text}"
         break
     done
 
-    text="${wifi_text}${ethernet_text}"
+    if [[ $wifi_text == '' ]]; then
+        text="${ethernet_text}"
+    elif [[ $ethernet_text == '' ]]; then
+        text="${wifi_text}"
+    else
+        text="${wifi_text}  |  ${ethernet_text}"
+    fi
 
     # clean up tooltip
     tooltip="${tooltip# | }"
